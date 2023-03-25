9e54e0d9e369cd36cba9f1f333a5884091caa8ed
Maksim Timofeev <maxf.f2014@yandex.ru>
2023-03-25 10:43:46 +0400

    add apolo

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
